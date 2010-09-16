620b057a2f548b7e9640b86f2c0ca4cda67e6406
Jussi Savolainen <ajussis@gmail.com>
2010-09-16 01:14:58 +0000

    adapter updated
    git-svn-id: svn://localhost/svn/knowledgeportals@116 65751657-eb06-4f00-b96c-b21d08089218

--- cip.sppolicy4/cip/sppolicy4/adapter.py
+++ cip.sppolicy4/cip/sppolicy4/adapter.py
@@ -21,6 +21,24 @@ class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
         return self.context.setMemberProperties({'gender': value})
     gender = property(get_gender, set_gender)
 
+    def get_mobile(self):
+        return self.context.getProperty('mobile', '')
+    def set_mobile(self, value):
+        return self.context.setMemberProperties({'mobile': value})
+    mobile = property(get_mobile, set_mobile)
+
+    def get_officen(self):
+        return self.context.getProperty('officen', '')
+    def set_officen(self, value):
+        return self.context.setMemberProperties({'officen': value})
+    officen = property(get_officen, set_officen)
+
+    def get_skype(self):
+        return self.context.getProperty('skype', '')
+    def set_skype(self, value):
+        return self.context.setMemberProperties({'skype': value})
+    skype = property(get_skype, set_skype)
+
     def get_birthdate(self):
         return self.context.getProperty('birthdate', '')
     def set_birthdate(self, value):
@@ -50,6 +68,30 @@ class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
     def set_newsletter(self, value):
         return self.context.setMemberProperties({'newsletter': value})
     newsletter = property(get_newsletter, set_newsletter)
+
+    def get_institution(self):
+        return self.context.getProperty('institution', '')
+    def set_institution(self, value):
+        return self.context.setMemberProperties({'institution': value})
+    institution = property(get_institution, set_institution)
+
+    def get_instadd(self):
+        return self.context.getProperty('instadd', '')
+    def set_instadd(self, value):
+        return self.context.setMemberProperties({'instadd': value})
+    instadd = property(get_instadd, set_instadd)
+
+    def get_position(self):
+        return self.context.getProperty('position', '')
+    def set_position(self, value):
+        return self.context.setMemberProperties({'position': value})
+    position = property(get_position, set_position)
+
+    def get_profession(self):
+        return self.context.getProperty('profession', '')
+    def set_profession(self, value):
+        return self.context.setMemberProperties({'profession': value})
+    profession = property(get_profession, set_profession)
 
     def get_accept(self):
         return self.context.getProperty('accept', '')
