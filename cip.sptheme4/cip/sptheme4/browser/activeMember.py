979be64094f4f0aaa527d1ff9a46acc91e0c4f44
Jussi Savolainen <ajussis@gmail.com>
2010-12-09 05:57:47 +0000

    mostactives.pt updated, another tab created
    git-svn-id: svn://localhost/svn/knowledgeportals@137 65751657-eb06-4f00-b96c-b21d08089218

--- cip.sptheme4/cip/sptheme4/browser/activeMember.py
+++ cip.sptheme4/cip/sptheme4/browser/activeMember.py
@@ -50,3 +50,47 @@ class ActiveMember(BrowserView):
 #        pdb.set_trace()
 #        creator = self.context.Creator()
         return contentAll
+
+    def activemembersByName(self):
+        """
+        """
+        buffer = ""
+        contentAll = []
+        # Returns list of site usernames
+        #users = self.context.acl_users.getUserNames()
+        contentlenght = []
+        users = self.context.acl_users.getUserIds()
+
+#        for userId in users:
+#            author_content = len(self.context.author_find_content(userId))
+#            pImg = self.context.portal_membership.getPersonalPortrait(userId).tag()
+#            kk = pImg.find('" alt')
+#            returnImg = pImg[10:kk]
+#            contentAll.append([userId,author_content, returnImg])
+#            print contentAll"""
+
+        userLoad = []
+        contentAll = []
+        for userId in users:
+            author_content = len(self.context.author_find_content(userId)) - 1
+            userLoad.append([userId, author_content])
+#        import ipdb; ipdb.set_trace()
+
+        usersSorted = sorted(userLoad, key=lambda user: user[0], reverse=True)
+        acl_users = getToolByName(self.context, 'acl_users')
+
+        for userId in usersSorted:
+#            userid = acl_users.getUserById(userId)
+#            member_name = userid.getProperty('fullname')
+            userName = userId[0]
+            pImg = self.context.portal_membership.getPersonalPortrait(userName).tag()
+            kk = pImg.find('" alt')
+            returnImg = pImg[10:kk]
+            if userId[1] == -1:
+                userId[1] = 0
+            contentAll.append([userId[0],userId[1], returnImg])
+#        import pdb
+#        pdb.set_trace()
+#        creator = self.context.Creator()
+        contentAll.sort()
+        return contentAll
