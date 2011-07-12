3d94c53cd5580371a8e8c6ecabaf91f92e90209a
Jussi Savolainen <ajussis@gmail.com>
2011-07-12 01:36:24 +0000

    member xmls added
    git-svn-id: svn://localhost/svn/knowledgeportals@177 65751657-eb06-4f00-b96c-b21d08089218

---
+++ cip.potatopolicy/cip/potatopolicy/adapter.py
@@ -0,0 +1,101 @@
+from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
+
+class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
+    """
+    """
+    def get_firstname(self):
+        return self.context.getProperty('firstname', '')
+    def set_firstname(self, value):
+        return self.context.setMemberProperties({'firstname': value})
+    firstname = property(get_firstname, set_firstname)
+
+    def get_lastname(self):
+        return self.context.getProperty('lastname', '')
+    def set_lastname(self, value):
+        return self.context.setMemberProperties({'lastname': value})
+    lastname = property(get_lastname, set_lastname)
+
+    def get_gender(self):
+        return self.context.getProperty('gender', '')
+    def set_gender(self, value):
+        return self.context.setMemberProperties({'gender': value})
+    gender = property(get_gender, set_gender)
+
+    def get_mobile(self):
+        return self.context.getProperty('mobile', '')
+    def set_mobile(self, value):
+        return self.context.setMemberProperties({'mobile': value})
+    mobile = property(get_mobile, set_mobile)
+
+    def get_officen(self):
+        return self.context.getProperty('officen', '')
+    def set_officen(self, value):
+        return self.context.setMemberProperties({'officen': value})
+    officen = property(get_officen, set_officen)
+
+    def get_skype(self):
+        return self.context.getProperty('skype', '')
+    def set_skype(self, value):
+        return self.context.setMemberProperties({'skype': value})
+    skype = property(get_skype, set_skype)
+
+    def get_birthdate(self):
+        return self.context.getProperty('birthdate', '')
+    def set_birthdate(self, value):
+        return self.context.setMemberProperties({'birthdate': value})
+    birthdate = property(get_birthdate, set_birthdate)
+
+    def get_city(self):
+        return self.context.getProperty('city', '')
+    def set_city(self, value):
+        return self.context.setMemberProperties({'city': value})
+    city = property(get_city, set_city)
+
+    def get_country(self):
+        return self.context.getProperty('country', '')
+    def set_country(self, value):
+        return self.context.setMemberProperties({'country': value})
+    country = property(get_country, set_country)
+
+    def get_phone(self):
+        return self.context.getProperty('phone', '')
+    def set_phone(self, value):
+        return self.context.setMemberProperties({'phone': value})
+    phone = property(get_phone, set_phone)
+
+    def get_newsletter(self):
+        return self.context.getProperty('newsletter', '')
+    def set_newsletter(self, value):
+        return self.context.setMemberProperties({'newsletter': value})
+    newsletter = property(get_newsletter, set_newsletter)
+
+    def get_institution(self):
+        return self.context.getProperty('institution', '')
+    def set_institution(self, value):
+        return self.context.setMemberProperties({'institution': value})
+    institution = property(get_institution, set_institution)
+
+    def get_instadd(self):
+        return self.context.getProperty('instadd', '')
+    def set_instadd(self, value):
+        return self.context.setMemberProperties({'instadd': value})
+    instadd = property(get_instadd, set_instadd)
+
+    def get_position(self):
+        return self.context.getProperty('position', '')
+    def set_position(self, value):
+        return self.context.setMemberProperties({'position': value})
+    position = property(get_position, set_position)
+
+    def get_profession(self):
+        return self.context.getProperty('profession', '')
+    def set_profession(self, value):
+        return self.context.setMemberProperties({'profession': value})
+    profession = property(get_profession, set_profession)
+
+    def get_accept(self):
+        return self.context.getProperty('accept', '')
+    def set_accept(self, value):
+        return self.context.setMemberProperties({'accept': value})
+    accept = property(get_accept, set_accept)
+
---
+++ cip.potatopolicy/cip/potatopolicy/userdataschema2.py
@@ -0,0 +1,119 @@
+from zope.interface import Interface, implements
+from zope import schema
+
+from cip.potatopolicy import _
+from plone.app.users.userdataschema import IUserDataSchemaProvider
+from plone.app.users.userdataschema import IUserDataSchema
+
+def validateAccept(value):
+    if not value == True:
+        return False
+    return True
+
+class UserDataSchemaProvider2(object):
+    implements(IUserDataSchemaProvider)
+
+    def getSchema(self):
+        """
+        """
+        return IEnhancedUserDataSchema
+
+class IEnhancedUserDataSchema(IUserDataSchema):
+    """ Use all the fields from the default user data schema, and add various
+    extra fields.
+    """
+    firstname = schema.TextLine(
+        title=_(u'label_firstname', default=u'First name'),
+        description=_(u'help_firstname',
+                      default=u"Fill in your given name."),
+        required=True,
+        )
+    lastname = schema.TextLine(
+        title=_(u'label_lastname', default=u'Last name'),
+        description=_(u'help_lastname',
+                      default=u"Fill in your surname or your family name."),
+        required=True,
+        )
+    gender = schema.Choice(
+        title=_(u'label_gender', default=u'Gender'),
+        description=_(u'help_gender',
+                      default=u"Male / Female?"),
+        values = ['Male', 'Female'],
+        required=True,
+        )
+#    birthdate = schema.Date(
+#        title=_(u'label_birthdate', default=u'birthdate'),
+#        description=_(u'help_birthdate',
+#            default=u'Your date of birth, in the format dd-mm-yyyy'),
+#        required=False,
+#        )
+    mobile = schema.TextLine(
+        title=_(u'label_mobile', default=u'Mobile'),
+        description=_(u'help_mobile',
+                      default=u"Fill in your mobile number."),
+        required=False,
+        )
+    officen = schema.TextLine(
+        title=_(u'label_officen', default=u'Office number'),
+        description=_(u'help_officen',
+                      default=u"Fill in your office number."),
+        required=False,
+        )
+    skype = schema.TextLine(
+        title=_(u'label_skype', default=u'Skype account'),
+        description=_(u'help_skype',
+                      default=u"Fill in your skype account."),
+        required=False,
+        )
+    country = schema.TextLine(
+        title=_(u'label_country', default=u'Country'),
+        description=_(u'help_country',
+                      default=u"Fill in the country you live in."),
+        required=False,
+        )
+    city = schema.TextLine(
+        title=_(u'label_city', default=u'City'),
+        description=_(u'help_city',
+                      default=u"Fill in the city you live in."),
+        required=False,
+        )
+    institution = schema.TextLine(
+        title=_(u'label_institution', default=u'Institution / Organization'),
+        description=_(u'help_institution',
+                      default=u"Fill in the institution where you work."),
+        required=False,
+        )
+    instadd = schema.TextLine(
+        title=_(u'label_instadd', default=u'Institution address'),
+        description=_(u'help_instadd',
+                      default=u"Fill in the address of the institution where you work."),
+        required=False,
+        )
+    position = schema.TextLine(
+        title=_(u'label_position', default=u'Current position'),
+        description=_(u'help_instadd',
+                      default=u"Fill in the current position."),
+        required=False,
+        )
+    profession = schema.TextLine(
+        title=_(u'label_profession', default=u'Profession'),
+        description=_(u'help_profession',
+                      default=u"Fill in your profession."),
+        required=False,
+        )
+#    newsletter = schema.Bool(
+#        title=_(u'label_newsletter', default=u'Subscribe to newsletter'),
+#        description=_(u'help_newsletter',
+#                      default=u"If you tick this box, we'll subscribe you to "
+#                        "our newsletter."),
+#        required=False,
+#        )
+#    accept = schema.Bool(
+#        title=_(u'label_accept', default=u'Accept terms of use'),
+#        description=_(u'help_accept',
+#                      default=u"Tick this box to indicate that you have found,"
+#                      " read and accepted the terms of use for this site. "),
+#        required=True,
+#        constraint=validateAccept,
+#        )
+
