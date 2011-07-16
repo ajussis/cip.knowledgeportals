cc86b81aea5033e83c55dd31b182ed225a1b6da6
Jussi Savolainen <ajussis@gmail.com>
2011-07-16 22:35:22 +0000

    sptheme4 deleted
    git-svn-id: svn://localhost/svn/knowledgeportals@181 65751657-eb06-4f00-b96c-b21d08089218

--- cip.sptheme4/cip/sptheme4/browser/pathbar.py
+++
@@ -1,5 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets import common
-
-class PathBarViewlet(common.PathBarViewlet):
-    index = ViewPageTemplateFile('templates/path_bar.pt')
